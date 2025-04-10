cfea45ed3a218a3c13163cf82258fa6a49901f01
Alex Suglio <129419995+alexandersuglio@users.noreply.github.com>
2025-04-10 19:16:10 -0400

    Update built-in-methods.py

--- built-in-methods.py
+++ built-in-methods.py
@@ -9,3 +9,34 @@ print(hi("Alex"))
 evenOrOdd = lambda y : "even" if y % 2 == 0 else "odd"
 print(evenOrOdd(37))
 
+# List Comprehensions
+test = ["hello" for i in range(10)]
+print(test)
+
+listA = [1,2,3,4,5]
+test = [listA[i] for i in range(len(listA))]
+print(test)
+
+listB = [[1,2,3],[4,5,6]]
+test = [listB[i][j] for i in range(len(listB)) for j in range(len(listB[0]))]
+print(test)
+
+test = [("X", i) if i % 2 == 0 else ("Y", i) for i in range(10)]
+print(test)
+
+test = [("X", i) for i in range(10) if i % 2 == 0]
+print(test)
+
+test = "X" if True else "Y"
+print(test)
+
+grade = 80
+reportcard = "doing well" if grade >= 70 else "not good"
+print(reportcard)
+
+# test = [("X", i) if i > 18 ("Y", i) elif i == 18 ("Z", i) if i < 18 for i in range(20)]
+result = [i for i in range(20) if i % 3 == 0 and i % 5 == 0]
+print(result)
+
+result = [i for i in range(20) if i % 3 == 0 or i % 5 == 0]
+print(result)  
