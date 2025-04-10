b2e99a8e84e4c075bee5e1177c12b3e2e638663a
Alex Suglio <129419995+alexandersuglio@users.noreply.github.com>
2025-04-09 23:41:46 -0400

    Create built-in-methods.py

---
+++ built-in-methods.py
@@ -0,0 +1,11 @@
+# lambda practice
+x = lambda x : x * 2
+
+print(x(23))
+
+hi = lambda name : "Hello there " + name
+print(hi("Alex"))
+
+evenOrOdd = lambda y : "even" if y % 2 == 0 else "odd"
+print(evenOrOdd(37))
+
