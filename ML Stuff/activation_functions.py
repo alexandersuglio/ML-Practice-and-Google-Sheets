96dd60da600200758ad1a67443916a06ed2b2f88
Alex Suglio <129419995+alexandersuglio@users.noreply.github.com>
2025-04-15 23:49:09 -0400

    Update activation_functions.py

--- ML Stuff/activation_functions.py
+++ ML Stuff/activation_functions.py
@@ -1,1 +1,27 @@
+import math
+import numpy as np
+import random as rand
+
+test = np.random.rand(5)
+print(test)
+
+def sigmoid(x):
+
+    x = np.array(x)
+    return 1 / (1 + np.exp(-x))
+
+print(sigmoid(test))
+
+def softmax(x):
+
+  return np.exp(x) / np.sum(np.exp(x))
+
+print(softmax(test))
+
+def relu(x):
+
+  ans = np.maximum(x, test)
+  return ans
+
+print(relu(test))
 
