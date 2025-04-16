caf1db60225cc0f48c424740e849cbed5ff43455
Alex Suglio <129419995+alexandersuglio@users.noreply.github.com>
2025-04-15 23:55:20 -0400

    Update activation_functions.py

--- ML Stuff/activation_functions.py
+++ ML Stuff/activation_functions.py
@@ -5,6 +5,7 @@ import random as rand
 test = np.random.rand(5)
 print(test)
 
+# output range: (0,1) best used for Binary Classification output layer)
 def sigmoid(x):
 
     x = np.array(x)
@@ -12,12 +13,14 @@ def sigmoid(x):
 
 print(sigmoid(test))
 
+# output range: (0,1) sums to 1, multiclass classification
 def softmax(x):
 
   return np.exp(x) / np.sum(np.exp(x))
 
 print(softmax(test))
 
+# output range: [0,∞), best used for Most hidden layers in deep networks
 def relu(x):
 
   ans = np.maximum(x, test)
@@ -25,3 +28,6 @@ def relu(x):
 
 print(relu(test))
 
+# model type: Linear Regression   |  Activation Function: None
+#             Logistic Regression |  Activation Function: Sigmoid
+#             Neural Networks     |  Activation Function: ReLu (hidden layers), Sigmoid/Softmax (output layers)
