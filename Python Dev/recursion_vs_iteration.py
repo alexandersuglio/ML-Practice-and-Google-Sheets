b65f08791d0776b930afb9318bbc4360c0c96e2a
Alex Suglio <129419995+alexandersuglio@users.noreply.github.com>
2025-04-17 23:56:11 -0400

    Update recursion_vs_iteration.py

--- Python Dev/recursion_vs_iteration.py
+++ Python Dev/recursion_vs_iteration.py
@@ -20,3 +20,38 @@ def factorialR(n):
     return n * factorialR(n - 1)
 
 print(factorialR(5))
+
+def fib(n):
+
+  if n == 0:
+    return 0
+
+  if n == 1:
+    return 1
+
+  else:
+    return fib(n-1) + fib(n-2)
+
+print(fib(3))
+# 0 1 1 2 3 5 8
+
+def fib(n):
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+
+  twoBefore, oneBefore = 0, 1
+
+  for i in range(2, n + 1):
+
+    twoBefore, oneBefore = oneBefore, twoBefore + oneBefore
+    print(i,"th: ", twoBefore, oneBefore)
+
+  return oneBefore
+
+print(fib(6))
+
+# 0 1 2 3 4 5
+#     |      6
+#.    0 1
